dc88137883795022cc819e03ad77d4e5ee793972
OliveiraCleiton <oliveira.cleiton@outlook.com.br>
2024-08-04 07:48:56 -0300

    versao mais performatica exemplo 6 cap05

--- cap05/jsCap05/ex5_6.js
+++ cap05/jsCap05/ex5_6.js
@@ -5,16 +5,21 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const num = Number(frm.inNumero.value)
-    let numDivisores = 0
-    for (let i = 1; i <= num; i++) {
-        if (num % i == 0) {
-            numDivisores++
+
+    let temDivisor = 0
+
+    for (let i = 2; i <= num / 2; i++){
+        if(num % i == 0) {
+            temDivisor = 1
+            break
         }
     }
-    if (numDivisores == 2) {
-        resp.innerText = `${num} É primo`
+
+    if (num > 1 && !temDivisor){
+        resp.innerText = `${num} é primo`
     } else {
-        resp.innerText = `${num} Não é primo`
+        resp.innerText = `${num} não é primo`
     }
 
 })
+    
